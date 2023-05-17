fa6aaba0ae84e60a5b09b1630bc1bd106469e220
Gilles Felix <felix@novius.fr>
2023-05-17 13:51:59 +0200

    PublicationBadge

--- src/Nova/Fields/PublicationBadge.php
+++ src/Nova/Fields/PublicationBadge.php
@@ -3,18 +3,16 @@
 namespace Novius\LaravelNovaPublishable\Nova\Fields;
 
 use Laravel\Nova\Fields\Badge;
-use Novius\LaravelPublishable\Traits\Publishable;
 
 class PublicationBadge extends Badge
 {
     public function __construct($name, $attribute = null, callable $resolveCallback = null)
     {
         parent::__construct($name, function () {
-            /** @var Publishable $this */
-            if ($this->isPublished()) {
+            if ($this->resource->isPublished()) {
                 return 'success';
             }
-            if ($this->willBePublished()) {
+            if ($this->resource->willBePublished()) {
                 return 'warning';
             }
 
